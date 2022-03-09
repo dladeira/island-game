3e16de10a105f99ca69149524de2b27a65ca5581
dladeira <dladeiratx@gmail.com>
2022-03-09 18:14:53 +0100

    Added swinging weapons

---
+++ Assets/Scripts/Game/PlayerScripts/PlayerCurrentWeapon.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerCurrentWeapon : MonoBehaviour
+{
+    PlayerHotbar hotbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwingWeapon() {
+        Debug.Log("swinging weapon");
+    }
+}
--- Assets/Scripts/Game/PlayerScripts/PlayerHotbar.cs
+++ Assets/Scripts/Game/PlayerScripts/PlayerHotbar.cs
@@ -58,6 +58,8 @@ public class PlayerHotbar : NetworkBehaviour, IGameInventory
             {
                 SetEquippedItem(5);
             }
+
+            anim.SetBool("IsSwinging", Input.GetMouseButton(0));
         }
     }
 
